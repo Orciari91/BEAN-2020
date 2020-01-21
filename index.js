0a833b764ce4683216ea6a9b1f22d63061069aa2
Orciari91 <pablo.orciari@hotmail.com.ar>
2020-01-21 20:29:25 -0300

    Implementando body-parser

--- index.js
+++ index.js
@@ -1,13 +1,22 @@
 const express = require("express")
-
+const bodyParser = require("body-parser")
 const server = express()
-
 const port = 80
+const public = express.static("public")
+const json = bodyParser.json()
+const urlencoded = bodyParser.urlencoded({extended : false})
+
+
+/* Buscar archivos estaticos en el directorio /public */
+server.use(public)
+server.use(json)
+server.use(urlencoded)
 
 server.listen(port)
 
+/* Ejecutar endpoints customizados */
 server.post("/enviar", function(request, response){
-	console.log(request.data)
-	response.end("Estos son los datos enviados: ")
+	console.log(request.body)
+	response.end("Estos son los datos enviados: MIRAR LA CONSOLA")
 })
 
