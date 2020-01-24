19dfe5d39b5834bf552cc014a191c96225ddad22
Orciari91 <pablo.orciari@hotmail.com.ar>
2020-01-23 21:57:14 -0300

    validaciones

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express")
 const bodyParser = require("body-parser")
 const nodemailer = require("nodemailer")
+const multer = require("multer")
 
 /* INICIO configs nodemailer */
 
@@ -34,12 +35,14 @@ const port = 80
 const public = express.static("public")
 const json = bodyParser.json()
 const urlencoded = bodyParser.urlencoded({extended : false})
+const upload = multer()
 
 
 /* Buscar archivos estaticos en el directorio /public */
 server.use(public)
 server.use(json)
 server.use(urlencoded)
+server.use(upload.array())
 
 server.listen(port)
 
@@ -50,29 +53,30 @@ server.post("/enviar", function(request, response){
 		consulta: request.body
 	}
 
-	//Validación
+	//Validación INSTALAR MODULO JOI E IMPLEMENTARLO POR LOS IF's PARA VALIDAR ESQUEMA DE DATOS
+	//URL: https://github.com/hapijs/joi
 
-	if (datos.consulta.nombre == "") {
+	if (datos.consulta.nombre == "" || datos.consulta.nombre == null || datos.consulta.nombre == undefined) {
 		response.json({
 			rta: "error",
 			msg: "El nombre no puede quedar vacio"
 		})
 	}
-	else if (datos.consulta.correo == "" || datos.consulta.correo.indexOf("@") == -1)
+	else if (datos.consulta.correo == "" || datos.consulta.correo.indexOf("@") == -1 || datos.consulta.correo == null || datos.consulta.correo == undefined)
 	{
 		response.json({
 			rta: "error",
 			msg: "Ingrese un correo valido"
 		})
 	}
-	else if (datos.consulta.asunto == "")
+	else if (datos.consulta.asunto == "" || datos.consulta.asunto == null || datos.consulta.asunto == undefined)
 	{
 		response.json({
 			rta: "error",
 			msg: "Elija un asunto"
 		})	
 	}
-	else if (datos.consulta.mensaje.length < 50 || datos.consulta.mensaje.length > 200)
+	else if (datos.consulta.mensaje.length < 50 || datos.consulta.mensaje.length > 200 || datos.consulta.mensaje == null || datos.consulta.mensaje == undefined)
 	{
 		response.json({
 			rta: "error",
